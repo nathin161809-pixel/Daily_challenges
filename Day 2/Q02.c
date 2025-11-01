a7431f438d3176c0def24f35ecb0b73585db1a69
nathin161809-pixel <nathin161809@gmail.com>
2025-11-01 19:57:26 +0530

    Add files via upload

---
+++ Day 2/Q02.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main(){
+	int a;
+	scanf("%d",&a);
+	printf("Character of %d=%c",a,a);
+	return 0;
+}
---
+++ Day 2/Q05.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+	int a;
+	char b;
+	float c;
+	double d;
+	printf("%d\n%d\n%d\n%d",sizeof(int),sizeof(char),sizeof(float),sizeof(double));
+	return 0;
+}
