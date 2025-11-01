adbbdad2dabd2b4685cb6701ec28afbb84552ed4
nathin161809-pixel <nathin161809@gmail.com>
2025-11-01 19:59:57 +0530

    Add files via upload

---
+++ Day 3/Q01.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+	int a,b;
+	scanf("%d%d",&a,&b);
+	printf("Addition:%d\n",a+b);
+	printf("subraction:%d\n",a-b);
+	printf("division:%d\n",a/b);
+	printf("multiplication:%d\n",a*b);
+	printf("module:%d\n",a%b);
+	return 0;
+	
+}
